76fdf51801c2936a194e11572de61f5c16108336
tcoelhom <tiago.coelho-magalhaes@inria.fr>
2023-10-11 15:12:12 +0200

    back with osim to biomod after a while

--- example/example.py
+++ example/example.py
@@ -3,13 +3,13 @@ from osim_to_biomod import Converter, MuscleType, MuscleStateType
 if __name__ == "__main__":
     model_path = "Models/"
     converter = Converter(
-        model_path + "Wu_Shoulder_Model_via_points.bioMod",
-        model_path + "Wu_Shoulder_Model_via_points.osim",
+        model_path + "gait10dof18musc_updt_2.bioMod",
+        model_path + "gait10dof18musc_updt_2_no_pinjoint.osim",
         ignore_muscle_applied_tag=False,
         ignore_fixed_dof_tag=False,
         ignore_clamped_dof_tag=False,
         mesh_dir="Geometry",
-        muscle_type=MuscleType.HILL,
+        muscle_type=MuscleType.HILL_DE_GROOTE,
         state_type=MuscleStateType.DEGROOTE,
         print_warnings=True,
         print_general_informations=True,
